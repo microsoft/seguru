b0e91ae03c16affc71cd9a0c14318af4a682e250
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2025-05-21 20:57:29 +0000

    Remove unused gpu functions

--- crates/gpu/src/lib.rs
+++ crates/gpu/src/lib.rs
@@ -3,20 +3,13 @@
 #![register_tool(gpu_codegen)]
 use core::marker::PhantomData;
 
+/// This is motivated by thread::scope in Rust std lib.
 pub struct Scope<'scope, 'env: 'scope> {
     /// Invariance over 'scope, to make sure 'scope cannot shrink,
     /// which is necessary for soundness.
     ///
     /// Without invariance, this would compile fine but be unsound:
     ///
-    /// ```compile_fail,E0373
-    /// gpu::thread::scope(|s| {
-    ///     s.spawn(|| {
-    ///         let a = String::from("abcd");
-    ///         s.spawn(|| println!("{a:?}")); // might run after `a` is dropped
-    ///     });
-    /// });
-    /// ```
     scope: PhantomData<&'scope mut &'scope ()>,
     env: PhantomData<&'env mut &'env ()>,
 }
@@ -62,7 +55,6 @@ pub struct ThreadScope<'scope, 'env: 'scope> {
     env: PhantomData<&'env mut &'env ()>,
 }
 
-pub struct Thread;
 pub struct Dim {
     pub x: usize,
     pub y: usize,
@@ -79,66 +71,10 @@ impl<'scope, 'env: 'scope> ThreadScope<'scope, 'env> {
     }
 }
 
-impl Thread {
-    // Returns the block id, i.e. the index of the current block within the grid along the x, y, or z dimension.
-    // MLIR: gpu.block_id
-    #[no_mangle]
-    pub fn block_id(&self) -> Dim {
-        unimplemented!()
-    }
-
-    // Returns the number of threads in the thread block (aka the block size) along the x, y, or z dimension.
-    // MLIR: gpu.block_dim
-    #[no_mangle]
-    pub fn block_dim(&self) -> Dim {
-        unimplemented!()
-    }
-
-    // Returns the number of thread blocks in the grid along the x, y, or z dimension.
-    #[no_mangle]
-    pub fn grid_dim(&self) -> Dim {
-        unimplemented!()
-    }
-
-    // Returns the thread id, i.e. the index of the current thread within the block along the x, y, or z dimension.
-    // MLIR: gpu.block_dim
-    #[no_mangle]
-    pub fn local_thread(&self) -> Dim {
-        unimplemented!()
-    }
-
-    #[no_mangle]
-    pub fn global_invocation(&self) -> Dim {
-        Dim {
-            x: self.block_id().x * self.block_dim().x + self.local_thread().x,
-            y: self.block_id().y * self.block_dim().y + self.local_thread().y,
-            z: self.block_id().z * self.block_dim().z + self.local_thread().z,
-        }
-    }
-
-    #[no_mangle]
-    pub fn local_thread_id(&self) -> usize {
-        self.local_thread().x
-            + self.local_thread().y * self.block_dim().x
-            + self.local_thread().z * self.block_dim().x * self.block_dim().y
-    }
-
-    pub fn global_thread_id(&self) -> usize {
-        self.global_invocation().x
-            + self.global_invocation().y * self.grid_dim().x
-            + self.global_invocation().z * self.grid_dim().x * self.grid_dim().y
-    }
-}
-
 #[no_mangle]
 #[rustfmt::skip]
 pub fn sync() {
     // MLIR: gpu.sync
-    unimplemented!()
-}
-
-#[no_mangle]
-pub fn thread2() -> Thread {
     unimplemented!()
 }
 
@@ -148,6 +84,7 @@ pub enum DimType {
     Z,
 }
 
+/// Add a string attribute to the MLIR module.
 #[gpu_codegen::builtin(add_mlir_string_attr)]
 pub fn add_mlir_string_attr(_: &'static str) -> usize {
     unimplemented!()
@@ -168,68 +105,4 @@ pub fn global_thread_id() -> usize {
     unimplemented!()
 }
 
-pub struct SharedWriteGuard<'a, T: ?Sized + 'a> {
-    dummy: PhantomData<T>,
-    _guard: &'a (),
-}
-
-impl<T: ?Sized> !Send for SharedWriteGuard<'_, T> {}
-
-unsafe impl<T: ?Sized + Sync> Sync for SharedWriteGuard<'_, T> {}
-
-impl<T: ?Sized> core::ops::Deref for SharedWriteGuard<'_, T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        unimplemented!()
-    }
-}
-
-impl<T: ?Sized> core::ops::DerefMut for SharedWriteGuard<'_, T> {
-    fn deref_mut(&mut self) -> &mut T {
-        unimplemented!()
-    }
-}
-
-pub struct SharedReadGuard<'a, T: ?Sized + 'a> {
-    dummy: PhantomData<T>,
-    _guard: &'a (),
-}
-
-impl<T: ?Sized> !Send for SharedReadGuard<'_, T> {}
-
-unsafe impl<T: ?Sized + Sync> Sync for SharedReadGuard<'_, T> {}
-
-impl<T: ?Sized> core::ops::Deref for SharedReadGuard<'_, T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        unimplemented!()
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-
-pub struct Shared<T> {
-    data: PhantomData<T>,
-}
-
-impl<T> Default for Shared<T> {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl<T> Shared<T> {
-    pub fn new() -> Self {
-        unimplemented!()
-    }
-
-    pub fn write(&self) -> SharedWriteGuard<'_, T> {
-        unimplemented!()
-    }
-
-    pub fn read(&self) -> SharedReadGuard<'_, T> {
-        unimplemented!()
-    }
-}
+/*  TODO: Define shared memory */

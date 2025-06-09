64014f4ca939179c300a095eaca92c60bbd346d2
Shixuan Zhao <shixuan.zhao@microsoft.com>
2025-06-09 16:56:02 -0700

    Modified to trigger fault with NULL ptr
    The reason of giving up divided by zero is because it *won't* trigger a
    fault in CUDA but instead will give an 'architectural specific value'.
    We have to switch back to the NULL ptr stuff. This is a local memory
    load on the stack and is volatile meaning that it won't be optimised
    away.
    
    To achieve this, a trait func emit_llvm_volatile_and_load is added. This
    is because only LLVM dialects supports volatile load. The pointer (which
    is still memref) will first be casted into an index, then i64 and then
    do the and with the OOB mask. It will then be casted into an LLVM ptr
    then used for the load.

--- compiler/rustc_codegen_ssa/src/mir/block.rs
+++ compiler/rustc_codegen_ssa/src/mir/block.rs
@@ -745,15 +745,14 @@ impl<'a, 'tcx, Bx: BuilderMethods<'a, 'tcx>> FunctionCx<'a, 'tcx, Bx> {
                 //     oob = index - len;    <- This will be positive if index is OOB
                 //     oob_flag = oob >> 63; <- Use signed shift right. If not OOB we
                 //                              get 0xFF....FF. If OOB we get 0
-                //     dummy = 1 / oob_flag; <- Divided by zero if OOB
+                //     *(dummy & oob_flag);  <- NULL ptr if OOB
                 let index_int = bx.intcast(index, bx.cx().type_i64(), true);
                 let oob = bx.sub(index_int, len);
                 let shift = bx
                     .emit_constant(63, bx.cx().backend_type(bx.cx().layout_of(bx.tcx().types.u64)));
                 let oob_flag = bx.ashr(oob, shift);
-                let one = bx
-                    .emit_constant(1, bx.cx().backend_type(bx.cx().layout_of(bx.tcx().types.u64)));
-                let dummy = bx.sdiv(one, oob_flag);
+
+                bx.emit_llvm_volatile_and_load(oob_flag, self.san_dummy.unwrap());
             }
             _ => {
                 ();

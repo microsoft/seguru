2f401ae3074d1599a7814812890476461313b3e4
Shixuan Zhao <shixuan.zhao@microsoft.com>
2025-06-20 14:44:43 -0700

    cuda_binding: add constant GPU_MEMCPY_{D2H, H2D}

--- crates/cuda_bindings/src/lib.rs
+++ crates/cuda_bindings/src/lib.rs
@@ -3,5 +3,8 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+pub const GPU_MEMCPY_D2H: u8 = 0;
+pub const GPU_MEMCPY_H2D: u8 = 1;
+
 // This matches bindgen::Builder output
 include!("./bindings.rs");

86afae67b2f13e69d77039f2eab5e7327f44397b
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2025-06-25 23:01:46 -0700

    Allow use of gpu and gpu_macro lib in unit tests

--- crates/rustc_codegen_gpu/tests/codegen_tests.rs
+++ crates/rustc_codegen_gpu/tests/codegen_tests.rs
@@ -1,8 +1,27 @@
 // tests/codegen_tests.rs
 
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use compiletest_rs as compiletest;
+
+// suffix is .so or .rmeta
+fn find_proc_macro_dylib(target_dir: &Path, crate_name: &str, suffix: &str) -> PathBuf {
+    let search_dir = target_dir.join("deps");
+
+    let entries = std::fs::read_dir(&search_dir).expect("Could not read deps dir");
+
+    for entry in entries.flatten() {
+        let file_name = entry.file_name();
+        let file_name = file_name.to_string_lossy();
+        if file_name.starts_with(&format!("lib{}-", crate_name.replace("-", "_")))
+            && file_name.ends_with(suffix)
+        {
+            return entry.path();
+        }
+    }
+
+    panic!("Proc macro dylib for '{}' not found in {:?}", crate_name, search_dir);
+}
 
 fn run_codegen_tests(src: PathBuf, mode: &str) {
     #[cfg(debug_assertions)]
@@ -12,19 +31,26 @@ fn run_codegen_tests(src: PathBuf, mode: &str) {
     let target_dir = std::env::var_os("CARGO_TARGET_DIR").map(PathBuf::from).unwrap_or_else(|| {
         PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../target").join(profile)
     });
-    let mut config = compiletest::Config::default();
-    config.mode = mode.parse().unwrap();
-    config.src_base = src.clone();
-    config.build_base = target_dir.join(src.as_os_str());
-    config.llvm_filecheck = Some(target_dir.join("filecheck"));
 
     // 👇 Point to your shared backend library
     let backend_path = target_dir.join("librustc_codegen_gpu.so");
-    config.target_rustcflags = Some(format!(
-        "-C opt-level=3 -Zcodegen-backend={} --crate-type=lib",
-        backend_path.display()
-    ));
-    println!("config.target_rustcflags: {}", config.target_rustcflags.as_ref().unwrap());
+    let gpu_macros_path = find_proc_macro_dylib(&target_dir, "gpu_macros", ".so");
+    let gpu_path = find_proc_macro_dylib(&target_dir, "gpu", ".rlib");
+
+    let config = compiletest_rs::Config {
+        mode: mode.parse().unwrap(),
+        src_base: src.clone(),
+        build_base: target_dir.join(src.as_os_str()),
+        llvm_filecheck: Some(target_dir.join("filecheck")),
+        target_rustcflags: Some(format!(
+            "-C opt-level=3 -Zcodegen-backend={} --crate-type=lib --extern gpu_macros={} --extern gpu={}",
+            backend_path.display(),
+            gpu_macros_path.display(),
+            gpu_path.display()
+        )),
+        ..Default::default()
+    };
+
     compiletest::run_tests(&config);
 }
 

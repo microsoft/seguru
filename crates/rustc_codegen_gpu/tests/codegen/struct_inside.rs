a50dc039beb7bfae7d565a854995206c13b4d366
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2025-06-30 16:59:32 -0700

    Add more tests with structs

--- crates/rustc_codegen_gpu/tests/codegen/struct_inside.rs
+++ crates/rustc_codegen_gpu/tests/codegen/struct_inside.rs
@@ -4,15 +4,24 @@
 #![register_tool(gpu_codegen)]
 
 #[repr(packed, C)]
+#[derive(Clone, Copy)]
 pub struct A {
     a: i32,
     b: u32,
+    d: u64,
 }
 
 #[no_mangle]
 #[gpu_codegen::kernel]
-pub fn assign_with_struct(b: &mut u32) {
-    let a = A { a: 1, b: 2 };
+pub fn assign_with_struct(b: &mut u32, c: &mut u32, aa: &A) {
+    let a = A { a: 1, b: 2, d: 3 };
+    *b = a.b;
+    *c = aa.b;
+}
+
+#[no_mangle]
+#[gpu_codegen::device]
+pub fn _assign_with_struct(b: &mut u32, a: A) {
     *b = a.b;
 }
 
---
+++ crates/rustc_codegen_gpu/tests/codegen_fails/struct_indirect_fnabi.rs
@@ -0,0 +1,18 @@
+// compile-flags: --emit=llvm-ir
+// compile-pass
+#![feature(register_tool)]
+#![register_tool(gpu_codegen)]
+
+#[repr(packed, C)]
+pub struct A {
+    a: i32,
+    b: u32,
+    c: u64,
+    d: f32,
+}
+
+#[no_mangle]
+#[gpu_codegen::kernel]
+pub fn assign_struct(a: A, b: &mut A) { //~ ERROR Kernel entry does not support fn abi indirect
+    *b = a;
+}

dd5dfba90f2804f2b4f6f4711a2fb3955e41dddd
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2025-06-29 21:45:16 -0700

    Define GPU thread lib and allow use of Chunk and scope combined to partition mutable vars
    scope function requires arguments are non-mutable and thus requires the
    use of chunk_mut-like function to create partitions.
    
    TODO: Add security checks via MIR analysis to avoid use of mutable
    argument outside of scope.

---
+++ crates/gpu/src/thread.rs
@@ -0,0 +1,44 @@
+/// This is motivated by thread::scope in Rust std lib.
+use core::marker::PhantomData;
+
+pub struct ThreadScope<'scope, 'env: 'scope> {
+    scope: PhantomData<&'scope mut &'scope ()>,
+    env: PhantomData<&'env mut &'env ()>,
+}
+
+#[gpu_codegen::builtin(gpu.scope)]
+#[gpu_codegen::device]
+#[inline(never)]
+pub fn scope<'env, F, T>(f: F) -> T
+where
+    F: for<'scope> FnOnce(&'scope ThreadScope<'scope, 'env>) -> T + Clone + Send,
+{
+    f(&ThreadScope { scope: PhantomData, env: PhantomData })
+}
+
+#[derive(Clone, Copy)]
+pub struct GpuChunksMut<'a, T: 'a> {
+    _ptr: &'a [T],
+    _chunk_size: usize,
+    _idx: crate::GpuChunkIdx,
+}
+
+// Assume T::index(i) function is injective
+impl<'a, T> GpuChunksMut<'a, T> {
+    #[inline(never)]
+    #[gpu_codegen::builtin(gpu.chunk_next)]
+    #[gpu_codegen::device]
+    pub fn next(&self) -> &'a mut [T] {
+        unimplemented!()
+    }
+}
+
+#[gpu_codegen::device]
+#[inline(never)]
+pub fn gpu_chunk_mut<'a, T>(
+    _ptr: &'a mut [T],
+    _chunk_size: usize,
+    _idx: crate::GpuChunkIdx,
+) -> GpuChunksMut<'a, T> {
+    GpuChunksMut { _ptr, _chunk_size, _idx }
+}

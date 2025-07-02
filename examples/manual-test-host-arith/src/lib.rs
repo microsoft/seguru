5ad6320ac199fb855e3df65b4c89d51639460e31
Shixuan Zhao <shixuan.zhao@microsoft.com>
2025-07-02 14:59:54 -0700

    manual-host: now uses macro to generate the launching wrapper

--- examples/manual-test-host-arith/src/lib.rs
+++ examples/manual-test-host-arith/src/lib.rs
@@ -17,75 +17,88 @@ mod internal {
     }
 }
 
+#[gpu_macros::host]
+fn kernel_arith(a: &[u8], b: &mut [u8]) {
+    let config = cuda_bindings::GPUConfig {
+        grid_dim_x: 1,
+        grid_dim_y: 1,
+        grid_dim_z: 1,
+        block_dim_x: 4,
+        block_dim_y: 1,
+        block_dim_z: 1,
+        shared_mem_bytes: 0,
+    };
+}
+
 // This wrapper should be generated automatically. Note that it should have known the
 // kernel function's name!
-fn kernel_launch_wrapper(a: &[u8], a_window: usize, b: &mut [u8], b_window: usize) -> i32 {
-    // Build the args array
-    let a_ptr = a.as_ptr() as *const ::std::os::raw::c_void;
-    let a_ptr_rptr: *const *const ::std::os::raw::c_void = &a_ptr;
-    let a_ptr_ptr = a_ptr_rptr as *const ::std::os::raw::c_void;
-    let a_size = a.len();
-    let a_size_rptr: *const usize = &a_size;
-    let a_size_ptr: *const ::std::os::raw::c_void = a_size_rptr as *const ::std::os::raw::c_void;
-    let a_window_rptr: *const usize = &a_window;
-    let a_window_ptr: *const ::std::os::raw::c_void =
-        a_window_rptr as *const ::std::os::raw::c_void;
-    let b_ptr = b.as_ptr() as *const ::std::os::raw::c_void;
-    let b_ptr_rptr: *const *const ::std::os::raw::c_void = &b_ptr;
-    let b_ptr_ptr = b_ptr_rptr as *const ::std::os::raw::c_void;
-    let b_size = b.len();
-    let b_size_rptr: *const usize = &b_size;
-    let b_size_ptr: *const ::std::os::raw::c_void = b_size_rptr as *const ::std::os::raw::c_void;
-    let b_window_rptr: *const usize = &b_window;
-    let b_window_ptr: *const ::std::os::raw::c_void =
-        b_window_rptr as *const ::std::os::raw::c_void;
-    let args: &mut [*const ::std::os::raw::c_void] = &mut [
-        a_ptr_ptr,
-        a_ptr_ptr,
-        a_size_ptr,
-        a_size_ptr,
-        a_size_ptr,
-        a_size_ptr,
-        a_window_ptr,
-        b_ptr_ptr,
-        b_ptr_ptr,
-        b_size_ptr,
-        b_size_ptr,
-        b_size_ptr,
-        b_size_ptr,
-        b_window_ptr,
-    ];
-
-    let func_name_cstr = std::ffi::CString::new("kernel_arith_wrapper").unwrap();
-    let func_name = func_name_cstr.as_ptr() as *const ::std::os::raw::c_char;
-    let args_ptr = args.as_mut_ptr() as *mut ::std::os::raw::c_void;
-
-    let mut res;
-    unsafe {
-        res = cuda_bindings::gpu_launch_kernel(
-            func_name,
-            1,
-            1,
-            1,
-            4,
-            1,
-            1,
-            0,
-            args_ptr,
-            core::ptr::null_mut(),
-        );
-    }
-
-    if res != 0 {
-        return res;
-    }
-
-    unsafe {
-        res = cuda_bindings::gpu_device_sync();
-    }
-
-    res
-}
+//fn kernel_launch_wrapper(a: &[u8], a_window: usize, b: &mut [u8], b_window: usize) -> i32 {
+//    // Build the args array
+//    let a_ptr = a.as_ptr() as *const ::std::os::raw::c_void;
+//    let a_ptr_rptr: *const *const ::std::os::raw::c_void = &a_ptr;
+//    let a_ptr_ptr = a_ptr_rptr as *const ::std::os::raw::c_void;
+//    let a_size = a.len();
+//    let a_size_rptr: *const usize = &a_size;
+//    let a_size_ptr: *const ::std::os::raw::c_void = a_size_rptr as *const ::std::os::raw::c_void;
+//    let a_window_rptr: *const usize = &a_window;
+//    let a_window_ptr: *const ::std::os::raw::c_void =
+//        a_window_rptr as *const ::std::os::raw::c_void;
+//    let b_ptr = b.as_ptr() as *const ::std::os::raw::c_void;
+//    let b_ptr_rptr: *const *const ::std::os::raw::c_void = &b_ptr;
+//    let b_ptr_ptr = b_ptr_rptr as *const ::std::os::raw::c_void;
+//    let b_size = b.len();
+//    let b_size_rptr: *const usize = &b_size;
+//    let b_size_ptr: *const ::std::os::raw::c_void = b_size_rptr as *const ::std::os::raw::c_void;
+//    let b_window_rptr: *const usize = &b_window;
+//    let b_window_ptr: *const ::std::os::raw::c_void =
+//        b_window_rptr as *const ::std::os::raw::c_void;
+//    let args: &mut [*const ::std::os::raw::c_void] = &mut [
+//        a_ptr_ptr,
+//        a_ptr_ptr,
+//        a_size_ptr,
+//        a_size_ptr,
+//        a_size_ptr,
+//        a_size_ptr,
+//        a_window_ptr,
+//        b_ptr_ptr,
+//        b_ptr_ptr,
+//        b_size_ptr,
+//        b_size_ptr,
+//        b_size_ptr,
+//        b_size_ptr,
+//        b_window_ptr,
+//    ];
+//
+//    let func_name_cstr = std::ffi::CString::new("kernel_arith_wrapper").unwrap();
+//    let func_name = func_name_cstr.as_ptr() as *const ::std::os::raw::c_char;
+//    let args_ptr = args.as_mut_ptr() as *mut ::std::os::raw::c_void;
+//
+//    let mut res;
+//    unsafe {
+//        res = cuda_bindings::gpu_launch_kernel(
+//            func_name,
+//            1,
+//            1,
+//            1,
+//            4,
+//            1,
+//            1,
+//            0,
+//            args_ptr,
+//            core::ptr::null_mut(),
+//        );
+//    }
+//
+//    if res != 0 {
+//        return res;
+//    }
+//
+//    unsafe {
+//        res = cuda_bindings::gpu_device_sync();
+//    }
+//
+//    res
+//}
 
 fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
@@ -155,7 +168,7 @@ fn main() -> ExitCode {
     }
 
     // Now do the kernel
-    if kernel_launch_wrapper(d_a, window, d_b, window) != 0 {
+    if launch_kernel_arith(d_a, window, d_b, window) != 0 {
         eprintln!("{}: failed to execute kernel", args[0]);
         return ExitCode::from(1);
     }

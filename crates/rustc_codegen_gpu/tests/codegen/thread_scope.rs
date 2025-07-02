2b25b31fbf859889a2ce3b6c8fb79a7f8cab4f29
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2025-07-01 17:36:34 -0700

    Add thread_scope failed test

--- crates/rustc_codegen_gpu/tests/codegen/thread_scope.rs
+++ crates/rustc_codegen_gpu/tests/codegen/thread_scope.rs
@@ -6,25 +6,30 @@
 #[gpu_codegen::device]
 #[no_mangle]
 /// assume BK * BK == number of threads in a block x axis.
-fn kernel(a: &[u8], b: &mut u8) {
+fn kernel(a: &[u8], b: &mut u8) -> u8 {
     let thread_id_x = gpu::thread_id(gpu::DimType::X);
     *b = a[thread_id_x];
+    *b
 }
 
 #[gpu_macros::kernel_v2]
 #[no_mangle]
 pub fn kernel_arith(a: &[u8], b: &mut [u8], window: usize) {
     let chunks: gpu::GpuChunksMut<'_, u8> = gpu::gpu_chunk_mut(b, window, gpu::GpuChunkIdx::new());
-    gpu::scope(|s| {
+    let val = gpu::scope(|s| {
         let c = chunks.next(s);
-        kernel(a, &mut c[0]);
+        kernel(a, &mut c[0])
     });
+    gpu::println!("setting b[?] = %u", val);
 
-    let chunks: gpu::GpuChunksMut<'_, u8> = gpu::gpu_chunk_mut(b, 2, gpu::GpuChunkIdx::new());
+    // TODO(datarace): The following code should be disallowed by MIR checker for global memory.
+    // Changing chunks should only be possible for shared memory since we can use _thread_sync() to ensure
+    // all threads in a block have finished using the prior chunk.
+    /* let chunks: gpu::GpuChunksMut<'_, u8> = gpu::gpu_chunk_mut(b, 2, gpu::GpuChunkIdx::new());
     gpu::scope(|s| {
         let c = chunks.next(s);
         kernel(a, &mut c[0]);
-    });
+    });*/
 }
 
 // CHECK: @gpu_bin_cst = internal constant
---
+++ crates/rustc_codegen_gpu/tests/codegen_fails/thread_scope_fails.rs
@@ -0,0 +1,23 @@
+#![feature(register_tool)]
+#![register_tool(gpu_codegen)]
+#![feature(stmt_expr_attributes)]
+#![no_std]
+
+#[gpu_codegen::device]
+#[no_mangle]
+/// assume BK * BK == number of threads in a block x axis.
+fn kernel(a: &[u8], b: &mut u8) {
+    let thread_id_x = gpu::thread_id(gpu::DimType::X);
+    *b = a[thread_id_x];
+}
+
+#[gpu_macros::kernel_v2]
+#[no_mangle]
+pub fn kernel_arith(a: &[u8], b: &mut [u8], window: usize) {
+    let chunks: gpu::GpuChunksMut<'_, u8> = gpu::gpu_chunk_mut(b, window, gpu::GpuChunkIdx::new());
+    let scope = gpu::scope(|s| {
+        s //~ ERROR lifetime may not live long enough
+    });
+    let c = chunks.next(scope);
+    kernel(a, &mut c[0]);
+}

888bf8e0077107766d27c3dc60836a097373e4e2
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2025-07-02 23:32:40 -0700

    Add failed tests

--- crates/rustc_codegen_gpu/tests/codegen_fails/shared_wrong_size.rs
+++ crates/rustc_codegen_gpu/tests/codegen_fails/shared_wrong_size.rs
@@ -7,21 +7,12 @@
 
 #[allow(non_upper_case_globals)]
 #[gpu_codegen::shared_size]
-pub static shared_size_alloc_shared: usize = 10;
+pub static shared_size_alloc_shared: usize = 9; //~ ERROR Shared memory size mismatch: expected 9, found 10
 
 #[gpu_macros::kernel_v2]
 #[no_mangle]
 pub fn alloc_shared(a: &[u8], _a_window: usize, b: &mut [u8], b_window: usize) {
     let mut shared: gpu::GpuShared::<[u8; 10]> = gpu::GpuShared::uninit();
-    let chunk_shared = gpu::chunk_mut(&mut *shared, 1, gpu::GpuChunkIdx::new());
-    let c = chunk_shared;
-    c[0] = a[gpu::thread_id(gpu::DimType::X)];
-    gpu::sync_threads();
-
-    let chunked_b = gpu::chunk_mut(b, 1, gpu::GpuChunkIdx::new());
-    for i in 0..10 {
-        chunked_b[0] += shared[i];
-    }
 }
 
 // CHECK: @gpu_bin_cst = internal constant

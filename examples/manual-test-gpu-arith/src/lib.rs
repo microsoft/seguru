19ccc33fa90ae871d18b44e0db89990afdf9155d
Shixuan Zhao <shixuan.zhao@microsoft.com>
2025-07-03 16:50:43 -0700

    manual-gpu: added shared mem size

--- examples/manual-test-gpu-arith/src/lib.rs
+++ examples/manual-test-gpu-arith/src/lib.rs
@@ -1,5 +1,9 @@
 #![no_std]
 
+#[allow(non_upper_case_globals)]
+#[gpu_codegen::shared_size]
+pub static shared_size_kernel_arith: usize = 0;
+
 #[gpu_macros::kernel]
 #[no_mangle]
 pub fn kernel_arith(a: &[u8], b: &mut [u8]) {

efed2fbcf31fe63df66ce2e0294cfb5e437c335f
Shixuan Zhao <shixuan.zhao@microsoft.com>
2025-07-10 17:04:03 -0700

    llm-rs-gpu: first kernel implemented porting llm.c to llm.rs
    This marks the first kernel ported from llm.c. There's a quirk about
    this kernel is that the out[xxx] stuff used calculation to get the index
    which turns out to just be that idx. Therefore our linear chunking will
    simply just work

---
+++ examples/llm-rs-gpu/src/lib.rs
@@ -0,0 +1,45 @@
+#![no_std]
+#![allow(non_snake_case)]
+#![allow(clippy::too_many_arguments)]
+
+use gpu::float4;
+
+#[gpu_macros::device]
+#[inline(always)]
+pub fn add_float4(a: &float4, b: &float4) -> float4 {
+    float4 {
+        x: a.x + b.x,
+        y: a.y + b.y,
+        z: a.z + b.z,
+        w: a.w + b.w,
+    }
+}
+
+#[gpu_macros::kernel]
+#[no_mangle]
+pub fn encoder_forward_kernel3(
+    out: &gpu::GpuChunkableMut<float4>,
+    inp: &[i32],
+    wte: &[float4],
+    wpe: &[float4],
+    B: i32,
+    T: i32,
+    C: i32,
+) {
+    let C4 = C / 4;
+    let idx = (gpu::block_dim(gpu::DimType::X) * gpu::block_id(gpu::DimType::X)
+        + gpu::thread_id(gpu::DimType::X)) as i32;
+    let N = B * T * C4;
+    if idx < N {
+        let bt = idx / C4;
+        let b = bt / T;
+        let t = bt % T;
+        let c4 = idx % C4;
+        let ix = inp[(b * T + t) as usize];
+
+        // bt = b * T + t, bt * C4 + c4 = idx
+        // b * T * C4 + t * C4 + c4 = idx
+        // out is local so we are at 0
+        out[0] = add_float4(&wte[(ix * C4 + c4) as usize], &wpe[(t * C4 + c4) as usize]);
+    }
+}

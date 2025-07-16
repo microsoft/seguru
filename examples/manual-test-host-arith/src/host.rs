80d2d31f7a449f668455fa8a9f0ecfecb0d00a07
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2025-07-15 22:44:42 -0700

    Improve manual-test-host-arith

---
+++ examples/manual-test-host-arith/src/host.rs
@@ -0,0 +1,108 @@
+#![allow(clippy::too_many_arguments)]
+
+mod internal {
+    /// Manually inserted function to test the host side API matches the GPU side API.
+    /// This should be generated automatically by the macro.
+    /// This is needed in order to force the compiler to link the GPU code.
+    #[allow(dead_code)]
+    fn dummy_api_checker_kernel_launch_wrapper(
+        a: &[u32],
+        a_window: usize,
+        b: &mut [u32],
+        b_window: usize,
+        c: &[u32],
+        f: &mut [f32],
+        f_window: usize,
+        g: &[f32],
+    ) {
+        manual_test_gpu_arith::kernel_arith(a, a_window, b, b_window, c, f, f_window, g);
+    }
+}
+
+#[gpu_macros::host(manual_test_gpu_arith::kernel_arith)]
+pub fn kernel_arith(
+    a: &gpu::GpuChunkable2D<u32>,
+    b: &gpu::GpuChunkableMut2D<u32>,
+    c: &[u32],
+    f: &gpu::GpuChunkableMut<f32>,
+    g: &[f32],
+) {
+    let config = cuda_bindings::GPUConfig {
+        grid_dim_x: 1,
+        grid_dim_y: 1,
+        grid_dim_z: 1,
+        block_dim_x: 1,
+        block_dim_y: 4,
+        block_dim_z: 1,
+    };
+}
+
+// This wrapper should be generated automatically. Note that it should have known the
+// kernel function's name!
+//fn kernel_launch_wrapper(a: &[u8], a_window: usize, b: &mut [u8], b_window: usize) -> i32 {
+//    // Build the args array
+//    let a_ptr = a.as_ptr() as *const ::std::os::raw::c_void;
+//    let a_ptr_rptr: *const *const ::std::os::raw::c_void = &a_ptr;
+//    let a_ptr_ptr = a_ptr_rptr as *const ::std::os::raw::c_void;
+//    let a_size = a.len();
+//    let a_size_rptr: *const usize = &a_size;
+//    let a_size_ptr: *const ::std::os::raw::c_void = a_size_rptr as *const ::std::os::raw::c_void;
+//    let a_window_rptr: *const usize = &a_window;
+//    let a_window_ptr: *const ::std::os::raw::c_void =
+//        a_window_rptr as *const ::std::os::raw::c_void;
+//    let b_ptr = b.as_ptr() as *const ::std::os::raw::c_void;
+//    let b_ptr_rptr: *const *const ::std::os::raw::c_void = &b_ptr;
+//    let b_ptr_ptr = b_ptr_rptr as *const ::std::os::raw::c_void;
+//    let b_size = b.len();
+//    let b_size_rptr: *const usize = &b_size;
+//    let b_size_ptr: *const ::std::os::raw::c_void = b_size_rptr as *const ::std::os::raw::c_void;
+//    let b_window_rptr: *const usize = &b_window;
+//    let b_window_ptr: *const ::std::os::raw::c_void =
+//        b_window_rptr as *const ::std::os::raw::c_void;
+//    let args: &mut [*const ::std::os::raw::c_void] = &mut [
+//        a_ptr_ptr,
+//        a_ptr_ptr,
+//        a_size_ptr,
+//        a_size_ptr,
+//        a_size_ptr,
+//        a_size_ptr,
+//        a_window_ptr,
+//        b_ptr_ptr,
+//        b_ptr_ptr,
+//        b_size_ptr,
+//        b_size_ptr,
+//        b_size_ptr,
+//        b_size_ptr,
+//        b_window_ptr,
+//    ];
+//
+//    let func_name_cstr = std::ffi::CString::new("kernel_arith_wrapper").unwrap();
+//    let func_name = func_name_cstr.as_ptr() as *const ::std::os::raw::c_char;
+//    let args_ptr = args.as_mut_ptr() as *mut ::std::os::raw::c_void;
+//
+//    let mut res;
+//    unsafe {
+//        res = cuda_bindings::gpu_launch_kernel(
+//            func_name,
+//            1,
+//            1,
+//            1,
+//            4,
+//            1,
+//            1,
+//            0,
+//            args_ptr,
+//            core::ptr::null_mut(),
+//        );
+//    }
+//
+//    if res != 0 {
+//        return res;
+//    }
+//
+//    unsafe {
+//        res = cuda_bindings::gpu_device_sync();
+//    }
+//
+//    res
+//}

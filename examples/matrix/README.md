8bf23896b8846ee5dbeddb90218a1ac649f04f75
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2025-08-11 18:13:23 -0700

    Explain the bound-checking perm and Iterator perm using matrix multiply example

---
+++ examples/matrix/README.md
@@ -0,0 +1,39 @@
+### Matrix Multiply
+
+### Baseline: inner_product_kernel(v1) + no-bound-check
+
+| Setup arg(2048, 16)           | Time(s)       |Relative overhead|
+|-------------------------------|---------------|--------|
+|v1 + no-bound-check	        |1.557330589    |baseline|
+|v1 + select-bound-check	    |1.574405571	|1%      |
+|v1 + if-else-trap-bound-check	|6.143659647	|294%    |
+|v1 + arith-immed-bound-check	|4.360865864	|180%    |
+|v2 + no-bound-check	        |1.64882646	    |6%      |
+|v2 + select-bound-check	    |1.648791268	|6%      |
+|v2 + if-else-trap-bound-check	|6.081392385	|291%    |
+|v2 + arith-immed-bound-check	|9.079779658	|483%    |
+|v3 + no-bound-check	        |1.621740153    |3%      |
+|v3 + select-bound-check	    |1.578538085	|1%      |
+|v3 + if-else-trap-bound-check	|6.155008352	|295%    |
+|v3 + arith-immed-bound-check	|3.802663921	|144%    |
+
+### if-else-trap vs arith-immed vs select 
+
+* Both if-else-trap and arith-immed are far more expensive than select-based inplace bound-checking. 
+
+* `if-else-trap` one has a relative stable overhead for both v1 and v2.
+This might due to that if-else-trap logic is easier to optimize out and so inner loop does not have bound checks when using if-else-trap.
+
+* arith-immed bound check has a unstable overhead depending on the implementation.
+arith-immed-bound-check is never optimized out in PTX optimization. When using arith-immed-bound-check v3 outperforms v2 and v1 since v3 will skip the inner bound check since Rust will skip bound check for the inner loop.
+
+
+### inner_product_kernel(v1) vs inner_product_kernel(v2)
+
+In theory, if we change inner_product_kernel(v1) to inner_product_kernel(v2), Rust will skip bound checking and so we might get a better performance. However, in practice, due to PTX optimization, both versions will skip bound checking for inner loop. inner_product_kernel(v2) is even more expensive than v1.
+
+v2 will generate less-optimized ptx code, while v1 will generate highly-optimized ptx code.
+
+Though ptx1 uses more registers, the parallelism benefits often outweigh the cost, ptx1 unrolls the loop partially (processing 4 elements per iteration).
+
+In both cases, we did not see the bound-checking for inner loop when calculating `sum` and they are generated in MLIR but erased after some optimization.
--- examples/matrix/matmul-gpu/src/lib.rs
+++ examples/matrix/matmul-gpu/src/lib.rs
@@ -6,36 +6,97 @@ pub static shared_size_inner_product_kernel: usize = 0;
 
 /// # Safety
 /// This kernel might be unsafe because it uses Chunkable::new that is not defined as trusted chunking func.
+#[cfg(feature = "v1")]
+#[gpu_macros::kernel_v2]
+#[no_mangle]
+pub fn inner_product_kernel(a: &[f32], b: &[f32], c: gpu::GpuChunkableMut2D<f32>, n: usize) {
+    let mut row = gpu::block_id(gpu::DimType::Y) * gpu::block_dim(gpu::DimType::Y)
+        + gpu::thread_id(gpu::DimType::Y);
+
+    let mut c = c;
+    for i in 0..((n - 1) / gpu::dim(gpu::DimType::Y) + 1) {
+        let mut col = gpu::block_id(gpu::DimType::X) * gpu::block_dim(gpu::DimType::X)
+            + gpu::thread_id(gpu::DimType::X);
+        for j in 0..((n - 1) / gpu::dim(gpu::DimType::X) + 1) {
+            if row < n && col < n {
+                let mut sum = 0.0;
+                let aa = &a[row * n..row * n + n];
+                let mut b_idx = col;
+                #[expect(clippy::needless_range_loop)]
+                for k in 0..aa.len() {
+                    sum += aa[k] * b[b_idx];
+                    b_idx += n;
+                }
+
+                // Here's the cream: updating c
+                let c_local = gpu::get_local_mut_2d::<f32>(&mut c, j, i);
+                *c_local = sum;
+            }
+            col += gpu::dim(gpu::DimType::X);
+        }
+        row += gpu::dim(gpu::DimType::Y);
+    }
+}
+
+/// # Safety
+/// This kernel might be unsafe because it uses Chunkable::new that is not defined as trusted chunking func.
+#[cfg(feature = "v2")]
 #[gpu_macros::kernel_v2]
 #[no_mangle]
 pub fn inner_product_kernel(a: &[f32], b: &[f32], c: gpu::GpuChunkableMut2D<f32>, n: usize) {
     let mut row = gpu::block_id(gpu::DimType::Y) * gpu::block_dim(gpu::DimType::Y)
         + gpu::thread_id(gpu::DimType::Y);
     let mut c = c;
-    let c_ref = &mut c;
-    let mut i = 0;
-    while i <= (n - 1) / gpu::dim(gpu::DimType::Y) {
+    for i in 0..((n - 1) / gpu::dim(gpu::DimType::Y) + 1) {
         let mut col = gpu::block_id(gpu::DimType::X) * gpu::block_dim(gpu::DimType::X)
             + gpu::thread_id(gpu::DimType::X);
-        let mut j = 0;
-        while j <= (n - 1) / gpu::dim(gpu::DimType::X) {
+        for j in 0..((n - 1) / gpu::dim(gpu::DimType::X) + 1) {
             if row < n && col < n {
                 let mut sum = 0.0;
-                let mut k = 0;
-                let aa = &a[row * n..row * n + n];
-                while k < n {
-                    sum += aa[k] * b[k * n + col];
-                    k += 1;
+                let mut aa = &a[row * n..row * n + n];
+                let mut b_idx = col;
+                for a in aa {
+                    sum += *a * b[b_idx];
+                    b_idx += n;
                 }
 
                 // Here's the cream: updating c
-                let c_local = gpu::get_local_mut_2d::<f32>(c_ref, j, i);
+                let c_local = gpu::get_local_mut_2d::<f32>(&mut c, j, i);
                 *c_local = sum;
             }
             col += gpu::dim(gpu::DimType::X);
-            j += 1;
         }
         row += gpu::dim(gpu::DimType::Y);
-        i += 1;
+    }
+}
+
+/// # Safety
+/// This kernel might be unsafe because it uses Chunkable::new that is not defined as trusted chunking func.
+#[cfg(feature = "v3")]
+#[gpu_macros::kernel_v2]
+#[no_mangle]
+pub fn inner_product_kernel(a: &[f32], b: &[f32], c: gpu::GpuChunkableMut2D<f32>, n: usize) {
+    let mut row = gpu::block_id(gpu::DimType::Y) * gpu::block_dim(gpu::DimType::Y)
+        + gpu::thread_id(gpu::DimType::Y);
+    let mut c = c;
+    for i in 0..((n - 1) / gpu::dim(gpu::DimType::Y) + 1) {
+        let mut col = gpu::block_id(gpu::DimType::X) * gpu::block_dim(gpu::DimType::X)
+            + gpu::thread_id(gpu::DimType::X);
+        for j in 0..((n - 1) / gpu::dim(gpu::DimType::X) + 1) {
+            if row < n && col < n {
+                let mut sum = 0.0;
+                let aa = gpu::iter::GpuIter::new(&a[row * n..row * n + n]);
+                let mut b_idx = col;
+                for a in aa {
+                    sum += *a * b[b_idx];
+                    b_idx += n;
+                }
+                // Here's the cream: updating c
+                let c_local = gpu::get_local_mut_2d::<f32>(&mut c, j, i);
+                *c_local = sum;
+            }
+            col += gpu::dim(gpu::DimType::X);
+        }
+        row += gpu::dim(gpu::DimType::Y);
     }
 }

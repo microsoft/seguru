5573776ed04ee40066f61d4f050a4d6fd025b914
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2025-09-10 06:40:37 +0000

    example/hello: Update hello example
    Signed-off-by: Ziqiao Zhou <ziqiaozhou@microsoft.com>

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -1,32 +1,22 @@
+#![allow(incomplete_features)]
+#![feature(generic_const_exprs)]
+use gpu::SafeGpuConfig;
+
 #[gpu_macros::kernel]
-pub fn kernel(input: &[f32; 1]) {
-    gpu::println!("Hello world... input = {}", input[0]);
+pub fn kernel(input: &[f32; Config::BDIM_X as _]) {
+    gpu::println!(
+        "Hello world... input = {}",
+        input[gpu::thread_id::<gpu::DimX>()]
+    );
 }
 
 #[gpu_macros::host(kernel)]
-pub fn host(input: &gpu_host::CudaMemBox<[f32; 1]>) {}
-
-#[gpu_macros::kernel]
-pub fn kernel2(input: &mut [f32]) {
-    let chunk = gpu::GlobalThreadChunk::new(input, gpu::MapLinearWithDim::<1>::new(1));
-    gpu::println!("Hello world... input = {}", chunk[10]);
-}
-
-#[gpu_macros::host(kernel2)]
-pub fn host2(input: &mut gpu_host::CudaMemBox<[f32]>) {}
+pub fn host(input: &gpu_host::CudaMemBox<[f32; Config::BDIM_X as _]>) {}
 
 fn main() {
     gpu_host::cuda_ctx(0, |ctx, m| {
         let input = ctx.new_gmem([1.01; 1]).expect("Failed to allocate input");
         let config = gpu_host::gpu_config!(1, 1, 1, 1, 1, 1, 0);
         host(config, ctx, m, input).expect("Failed to run host arithmetic");
-    });
-
-    gpu_host::cuda_ctx(0, |ctx, m| {
-        let input = ctx
-            .new_gmem_with_len(1, &[1.01])
-            .expect("Failed to allocate input");
-        let config = gpu_host::gpu_config!(1, 1, 1, 1, 1, 1, 0);
-        host2(config, ctx, m, input).expect("Failed to run host arithmetic");
     });
 }

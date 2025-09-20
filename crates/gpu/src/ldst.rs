4d4cd122be4916cac40fa3cb0ccd5cc86ffae9b3
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2025-09-20 01:38:54 +0000

    gpu/ldst: fix minor & support cg and cs

--- crates/gpu/src/ldst.rs
+++ crates/gpu/src/ldst.rs
@@ -1,5 +1,5 @@
-macro_rules! impl_ldcs {
-    ($fname: ident, $ty:ty, $p: literal) => {
+macro_rules! impl_ld {
+    ($fname: ident, $ty:ty, $c: literal, $t: literal) => {
         #[gpu_codegen::device]
         #[inline(always)]
         pub fn $fname(ptr: &$ty) -> $ty {
@@ -7,7 +7,7 @@ macro_rules! impl_ldcs {
             let ptr = ptr as *const $ty;
             unsafe {
                 core::arch::asm!(
-                    concat!("ld.global.cs.", $p, " {0:e}, [{1:r}];"),
+                    concat!("ld.global.", $c, ".", $t, " {0:e}, [{1:r}];"),
                     out(reg) ret,
                     in(reg) ptr,
                 );
@@ -17,19 +17,23 @@ macro_rules! impl_ldcs {
     };
 }
 
-impl_ldcs!(__ldcs_u32, u32, "u32");
-impl_ldcs!(__ldcs_i32, i32, "u32");
-impl_ldcs!(__ldcs_f32, f32, "f32");
+impl_ld!(__ldcs_u32, u32, "cs", "u32");
+impl_ld!(__ldcs_i32, i32, "cs", "u32");
+impl_ld!(__ldcs_f32, f32, "cs", "f32");
 
-macro_rules! impl_stcs {
-    ($fname: ident, $ty:ty, $p: literal) => {
+impl_ld!(__ldcg_u32, u32, "cg", "u32");
+impl_ld!(__ldcg_i32, i32, "cg", "u32");
+impl_ld!(__ldcg_f32, f32, "cg", "f32");
+
+macro_rules! impl_st {
+    ($fname: ident, $ty:ty, $c: literal, $t: literal) => {
         #[gpu_codegen::device]
         #[inline(always)]
         pub fn $fname(ptr: &mut $ty, val: $ty) {
             let ptr = ptr as *mut $ty;
             unsafe {
                 core::arch::asm!(
-                    concat!("st.global.cs.", $p, "[{0:r}], {1:e};"),
+                    concat!("st.global.", $c, ".", $t, "[{0:r}], {1:e};"),
                     in(reg) ptr,
                     in(reg) val,
                 );
@@ -38,14 +42,20 @@ macro_rules! impl_stcs {
     };
 }
 
-impl_ldcs!(__stcs_u32, u32, "u32");
-impl_stcs!(__stcs_i32, i32, "u32");
-impl_stcs!(__stcs_f32, f32, "f32");
+impl_st!(__stcs_u32, u32, "cs", "u32");
+impl_st!(__stcs_i32, i32, "cs", "u32");
+impl_st!(__stcs_f32, f32, "cs", "f32");
+
+impl_st!(__stcg_u32, u32, "cg", "u32");
+impl_st!(__stcg_i32, i32, "cg", "u32");
+impl_st!(__stcg_f32, f32, "cg", "f32");
 
 pub trait CacheStreamLoadStore: Sized {
     type Output;
     fn ldcs(&self) -> Self::Output;
     fn stcs(&mut self, val: Self::Output);
+    fn ldcg(&self) -> Self::Output;
+    fn stcg(&mut self, val: Self::Output);
 }
 
 impl CacheStreamLoadStore for i32 {
@@ -60,6 +70,18 @@ impl CacheStreamLoadStore for i32 {
     #[inline(always)]
     fn stcs(&mut self, val: Self::Output) {
         __stcs_i32(self, val)
+    }
+
+    #[gpu_codegen::device]
+    #[inline(always)]
+    fn ldcg(&self) -> Self::Output {
+        __ldcg_i32(self)
+    }
+
+    #[gpu_codegen::device]
+    #[inline(always)]
+    fn stcg(&mut self, val: Self::Output) {
+        __stcg_i32(self, val)
     }
 }
 
@@ -76,5 +98,17 @@ impl CacheStreamLoadStore for f32 {
     #[inline(always)]
     fn stcs(&mut self, val: Self::Output) {
         __stcs_f32(self, val)
+    }
+
+    #[gpu_codegen::device]
+    #[inline(always)]
+    fn ldcg(&self) -> Self::Output {
+        __ldcg_f32(self)
+    }
+
+    #[gpu_codegen::device]
+    #[inline(always)]
+    fn stcg(&mut self, val: Self::Output) {
+        __stcg_f32(self, val)
     }
 }

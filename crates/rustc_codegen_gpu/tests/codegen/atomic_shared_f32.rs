ead83c6cbc1d411b43044911d7d17e8ab17cdcbc
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2025-09-24 23:55:05 +0000

    codegen/tests: Add unit tests for SharedAtomic.
    Cover both success and failed tests.
    If switching from SharedAtomic to Non-atomic, we require sync_threads

---
+++ crates/rustc_codegen_gpu/tests/codegen/atomic_shared_f32.rs
@@ -0,0 +1,24 @@
+// compile-flags: --emit=llvm-ir --emit=mir
+// compile-pass
+#![feature(register_tool)]
+#![register_tool(gpu_codegen)]
+#![feature(stmt_expr_attributes)]
+#![no_std]
+
+#[gpu_macros::kernel(dynamic_shared)]
+#[no_mangle]
+pub fn test_atomic_shared(f: f32, b: &mut f32, mut smem_alloc: gpu::DynamicSharedAlloc) {
+    let mut smem = smem_alloc.alloc::<f32>(1);
+    let atomic_smem = gpu::sync::SharedAtomic::new(&mut smem);
+    atomic_smem.index(0).atomic_addf(f);
+    let b_atomic = gpu::sync::Atomic::new(b);
+    gpu::sync_threads();
+    b_atomic.atomic_addf(f);
+    b_atomic.atomic_addf(*smem[0]);
+}
+
+// CHECK: @gpu_bin_cst = internal constant
+// PTX_CHECK: .visible .entry atomic_
+// PTX_CHECK: atom.global.add.f32
+// PTX_CHECK: atom.shared.add.f32
+

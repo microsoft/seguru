791e02b6b02f23bc1fd257b8dba77f720c170967
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2025-12-05 08:29:05 +0000

    remove unstable generic_const_exprs
    Do not allow passing chunk from host to gpu

--- examples/syntax/host/src/host.rs
+++ examples/syntax/host/src/host.rs
@@ -4,7 +4,7 @@ use syntax_gpu::kernel_arith as kernel_arith_gpu;
 #[gpu::host(kernel_arith_gpu::<30>)]
 pub fn kernel_arith(
     a: &gpu_host::TensorView<'_, [u32]>,
-    b: gpu::GlobalThreadChunk<'_, u32, gpu::Map2D>,
+    b: &mut gpu_host::TensorViewMut<'_, [u32]>,
     c: &gpu_host::TensorView<'_, [u32]>,
     f: &mut gpu_host::TensorViewMut<'_, [f32]>,
     f_width: usize,
@@ -12,15 +12,3 @@ pub fn kernel_arith(
     h: &mut gpu_host::TensorViewMut<'_, f32>,
 ) {
 }
-
-#[gpu::host(syntax_gpu::oob1)]
-pub fn oob1(a: f32, b: &mut gpu_host::TensorViewMut<'_, [f32]>, width: usize) {}
-
-#[gpu::host(syntax_gpu::oob_no_fails)]
-pub fn oob_no_fails(a: f32, b: &mut gpu_host::TensorViewMut<'_, [f32]>, width: usize) {}
-
-#[gpu::host(syntax_gpu::oob2)]
-pub fn oob2(a: f32, b: &mut gpu_host::TensorViewMut<'_, [f32]>, width: usize) {}
-
-#[gpu::host(syntax_gpu::oob3)]
-pub fn oob3(a: f32, b: &mut gpu_host::TensorViewMut<'_, [f32]>, width: usize) {}

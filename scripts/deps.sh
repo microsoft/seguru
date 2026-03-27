b124df2abff022f73b5d0f303b533b78e6f6a6c3
ziqiaozhou <ziqiaozhou@microsoft.com>
2026-03-27 15:37:20 -0700

    Improve workflow and dep.sh to drop brew-based installation (#145)
    * Improve workflow and dep.sh
    
    * update install.md

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -4,14 +4,10 @@ if ! command -v ptxas &> /dev/null; then
 fi
 
 if ! command -v mlir-opt &> /dev/null; then
-    test -x /home/linuxbrew/.linuxbrew/bin/brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    if ! command -v brew &> /dev/null; then
-        echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
-        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    fi
-    test -x /home/linuxbrew/.linuxbrew/bin/mlir-opt || /home/linuxbrew/.linuxbrew/bin/brew install llvm@20
-    if ! command -v mlir-opt &> /dev/null; then
-        export PATH=`brew --prefix llvm@20`/bin:$PATH
-        export LD_LIBRARY_PATH=`brew --prefix`/lib:`brew --prefix llvm@20`/bin:$LD_LIBRARY_PATH
-    fi
+    wget https://apt.llvm.org/llvm.sh
+    chmod +x llvm.sh
+    sudo ./llvm.sh 20
+    sudo apt-get install libmlir-20-dev mlir-20-tools libpolly-20-dev
+    export PATH=/usr/lib/llvm-20/bin:$PATH
+    export LD_LIBRARY_PATH=/usr/lib/llvm-20/lib:$LD_LIBRARY_PATH
 fi
